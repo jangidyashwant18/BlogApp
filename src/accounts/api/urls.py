768afd1cfaacbfbb3bd94682a1126b219a974f02
yashwant <jangidyashwant@yahoo.com>
2019-03-11 17:10:53 +0530

    First commit

---
+++ src/accounts/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import re_path  
+from django.contrib import admin
+
+from .views import (
+    UserCreateAPIView,
+    UserLoginAPIView
+    )
+app_name='users-api'
+urlpatterns = [
+    re_path(r'^login/$', UserLoginAPIView.as_view(), name='login'),
+    re_path(r'^register/$', UserCreateAPIView.as_view(), name='register'),
+]
